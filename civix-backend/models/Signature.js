6d7699e9eabb8ce10e714f6808e1532a2e2f87b2
jsingh830 <jsingh8_be21@thapar.edu>
2026-01-23 17:36:43 +0530

    Remove duplicate civix-backend and civix-frontend folder

--- civix-backend/models/Signature.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const signatureSchema = new mongoose.Schema({
-  petition: { type: mongoose.Schema.Types.ObjectId, ref: 'Petition', required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Signature', signatureSchema);
